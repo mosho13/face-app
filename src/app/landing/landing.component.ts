1bf1bbadc1366aa19ca5ae8910b036c1460ece5c
Milos Lazarevic <mosho13@gmail.com>
2022-08-16 17:02:10 +0200

    mydatapart update

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,19 +1,21 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
-//import { Browser } from '@capacitor/browser';
-// import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
-import { Storage } from '@capacitor/storage';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { UsernamePasswordLoginModel } from '../classes/username-password-login-model.class';
+
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 import { AvailableResult, BiometryType, NativeBiometric } from "capacitor-native-biometric";
-import { UsernamePasswordLoginModel } from '../classes/username-password-login-model.class';
-// import { toModel } from '../utils/resource-model.util';
-import { map, catchError } from 'rxjs/operators';
-// import { ApiError } from '../classes/api-error';
-import { throwError } from 'rxjs';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Storage } from '@capacitor/storage';
 import * as CryptoJS from 'crypto-js';
-import SimpleCrypto from "simple-crypto-js"
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
 
 // export declaration not working, checking for now, needs to be fixed
 
@@ -36,8 +38,14 @@ import SimpleCrypto from "simple-crypto-js"
 })
 export class LandingComponent implements OnInit {
 
-  username = new FormControl('', [])
-  password = new FormControl('', [])
+  username = new FormControl('', []);
+  password = new FormControl('', []);
+  permissionToken = '';
+  forwardData: {
+    username: string,
+    password: string,
+    token: string,
+  }
 
   constructor(
     private elRef: ElementRef,
@@ -60,7 +68,7 @@ export class LandingComponent implements OnInit {
           (validCredentials) => {
             console.log(validCredentials);
             console.log(typeof validCredentials);
-            if (!validCredentials) {
+            if (validCredentials) {
               this.startBiometricauthentification(validCredentials)
             } else {
               this.showPopupDialog();
@@ -75,6 +83,47 @@ export class LandingComponent implements OnInit {
     }
 
   async ngOnInit() {
+    // Request permission to use push notifications
+    // iOS will prompt user and return if they granted permission or not
+    // Android will just grant without prompting
+    PushNotifications.requestPermissions().then(result => {
+      if (result.receive === 'granted') {
+        // Register with Apple / Google to receive push via APNS/FCM
+        PushNotifications.register();
+      } else {
+        // Show some error
+      }
+    });
+
+    //On success, we should be able to receive notifications
+    PushNotifications.addListener('registration',
+      (token: Token) => {
+        console.log('Push registration success, token: ' + token.value);
+        alert('Push registration success, token: ' + token.value);
+        this.setNotificationPermissionToken(token.value);
+      }
+    );
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
   }
 
   async showPopupDialog() {
@@ -132,10 +181,6 @@ export class LandingComponent implements OnInit {
     await Storage.set({ key: 'credentials', value: JSON.stringify({ username, password }) });
   }
 
-  async setAccessToken(model) {
-    await Storage.set({ key: 'ebox-access-token', value: JSON.stringify({ model }) });
-  }
-
   async getCredentials() {
     await Storage.get({ key: 'credentials' }).then(
       (value) => {
@@ -147,6 +192,32 @@ export class LandingComponent implements OnInit {
         alert('no credentials');
       }
     );
+  }
+
+  async setAccessToken(model) {
+    await Storage.set({ key: 'ebox-access-token', value: JSON.stringify({ model }) });
+  }
+
+  async setNotificationPermissionToken(model) {
+    await Storage.set({ key: 'notification-permission-token', value: JSON.stringify({ model }) });
+  }
+
+  // async getPermissionTokenold() {
+  //   await Storage.get({ key: 'notification-permission-token' }).then(
+  //     (response) => {
+  //       this.permissionToken = response.value;
+  //       return response.value;
+  //     },
+  //     (error) => {
+  //       alert('no credentials');
+  //     }
+  //   );
+  // }
+  async get(key: string): Promise<any> {
+    const item = await Storage.get({ key: key });
+    const value = JSON.parse(item.value);
+    this.permissionToken = value.model;
+    return this.permissionToken;
   }
 
   onLoginSubmit() {
@@ -197,28 +268,42 @@ export class LandingComponent implements OnInit {
 
   }
 
+  async notificationTokenGet() {
+    this.permissionToken = await this.get('notification-permission-token');
+  }
+
   handleInboxRedirection(validCredentials) {
     // const secretKey = "abc";
     // const simpleCrypto1 = new SimpleCrypto(secretKey);
     // const encryptedObject = simpleCrypto1.encrypt(validCredentials).toString();
     // console.log(encryptedObject);
     // const dec = simpleCrypto1.decrypt(encryptedObject);
-    const data = {validCredentials};
+    // const data = {validCredentials};
     // const strignified = CryptoJS.enc.Utf8.parse(JSON.stringify(data));
     // const strignified = JSON.stringify(data)
-    const ciphertext = CryptoJS.AES.encrypt(JSON.stringify(validCredentials), 'abc').toString();
-    const encoded  = encodeURIComponent(ciphertext.toString());
-    // console.log(ciphertext);
-    const bytes  = CryptoJS.AES.decrypt(encoded, 'abc').toString(CryptoJS.enc.Utf8);
-    console.log(bytes);
-    // const originalText = bytes.toString(CryptoJS.enc.Utf8);
-    // const browser = this.iab.create('http://ebox-datapart.edeja.com/datapart/handle-inbox-app-redirection', '_blank', 'location=no');
-    const browser = this.iab.create(`http://10.5.20.159:4000/handle-inbox-app-redirection?ciphertext=${encoded}`, '_blank', 'location=no');
-    browser.on('loadstart').subscribe(event => {
-      // browser.executeScript({ code: `localStorage.setItem('ebox-credentials', ${ciphertext})` });
-      browser.executeScript({code: 'console.log("foo")'});
-      // crypto_js__WEBPACK_IMPORTED_MODULE_1__.AES.decrypt(queryParams.ciphertext, 'abc').toString(crypto_js__WEBPACK_IMPORTED_MODULE_1__.enc.Latin1)
+    this.notificationTokenGet().then(()=> {
+      let forwardData = Object();
+      const token = this.permissionToken
+      const data = [validCredentials, token]
+      forwardData.password = validCredentials.value ? validCredentials.value.password : validCredentials.password;
+      forwardData.username = validCredentials.value ? validCredentials.value.username : validCredentials.username;
+      forwardData.token = this.permissionToken;
+      const ciphertext = CryptoJS.AES.encrypt(JSON.stringify(forwardData), 'Edeja $secret$').toString();
+      const encoded  = encodeURIComponent(ciphertext.toString());
+      // console.log(ciphertext);
+      const bytes  = CryptoJS.AES.decrypt(ciphertext, 'Edeja $secret$').toString(CryptoJS.enc.Utf8);
+      console.log(bytes);
+      // const originalText = bytes.toString(CryptoJS.enc.Utf8);
+      // const browser = this.iab.create(`http://ebox-datapart.edeja.com/datapart/handle-inbox-app-redirection?ciphertext=${encoded}`, '_blank', 'location=no');
+      const browser = this.iab.create(`http://10.5.20.159:4000/handle-inbox-app-redirection?ciphertext=${encoded}`, '_blank', 'location=no');
+    //   browser.on('loadstart').subscribe(event => {
+    //     // browser.executeScript({ code: `localStorage.setItem('ebox-credentials', ${ciphertext})` });
+    //     browser.executeScript({code: 'console.log("foo")'});
+    //     // crypto_js__WEBPACK_IMPORTED_MODULE_1__.AES.decrypt(queryParams.ciphertext, 'abc').toString(crypto_js__WEBPACK_IMPORTED_MODULE_1__.enc.Latin1)
+    // }
+    //   );
+   }).catch((err) => {
+    alert(`Can't fetch permission token`);
    });
-
   }
 }
